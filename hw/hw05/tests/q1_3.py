25b292504b4c8f9a72356d2db356804e7996d44c
Vanessa Jaime <78834107+vanessa2213@users.noreply.github.com>
2022-01-26 11:55:48 -0600

    Add files via upload

---
+++ hw/hw05/tests/q1_3.py
@@ -0,0 +1,26 @@
+test = {
+  'name': 'Question 1_3',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> type(reasonable_test_statistics) == list
+          True
+          >>> len(reasonable_test_statistics) > 0
+          True
+          >>> reasonable_test_statistics == [3, 4, 6]
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
