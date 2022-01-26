bf1f82bd000169573b155eb0a0b0ea869186435b
Vanessa Jaime <78834107+vanessa2213@users.noreply.github.com>
2022-01-26 11:58:57 -0600

    Add files via upload

---
+++ labs/lab0/tests/q1.py
@@ -0,0 +1,22 @@
+test = {
+  'name': 'Question 1_',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> 1 <= answer <= 1
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
